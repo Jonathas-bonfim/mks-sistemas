307cb72be275ff1095f777d00f10ad267d8cf474
Jônathas Bonfim <jonathas.santos.bonfim@gmail.com>
2024-04-25 16:31:28 -0300

    feat/jb: stylizedheader

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,33 @@
+import { HeaderContainer } from "./styles";
+
+import mksImage from '../../assets/images/MKS.svg';
+import sistemasImage from '../../assets/images/Sistemas.svg';
+
+import cartIcon from '../../assets/icons/cart.svg';
+
 export function Header() {
   return (
-    <header>Header</header>
+    <HeaderContainer>
+      <main className="container-center">
+        <section className="logo">
+          <img
+            src={mksImage}
+            alt=""
+            className="mks-image"
+          />
+          <img
+            src={sistemasImage}
+            alt=""
+            className="sistemas-image"
+          />
+        </section>
+        <section className="section-cart">
+          <button>
+            <img src={cartIcon} alt="" />
+            <p>2</p>
+          </button>
+        </section>
+      </main>
+    </HeaderContainer>
   )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background-color: ${(props) => props.theme.colors.blue};
+
+  height: 6.312rem;
+
+  .container-center {
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+
+    height: 100%;
+  }
+
+  .logo {
+    .mks-image {
+      margin-right: 0.375rem;
+    }
+  }
+
+  .section-cart {
+    display: flex;
+
+    width: 5.625rem;
+    height: 2.812rem;
+
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+
+      border-radius: 8px;
+      border: none;
+      height: 100%;
+      width: 100%;
+    }
+  }
+`;
